6d89233b845ced9c1b2be9c667c045781b3122f5
b3rg3t <b3rg3t@gmail.com>
2025-12-13 11:31:35 +0100

    add husky and lint-staged

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import { createTheme } from "@mui/material/styles";
+import React from "react";
 
 const theme = createTheme({
   palette: {
